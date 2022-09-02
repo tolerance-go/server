9622e353a1eec8fa459387d6913189001ea70137
tolerance-go <892728595@qq.com>
2022-09-02 22:36:07 +0800

    可以安装库

--- view/src/models/widgetsMarket/tableList/widgetLibs.ts
+++ view/src/models/widgetsMarket/tableList/widgetLibs.ts
@@ -1,19 +1,19 @@
+import { useGetImmer } from '@/pages/Design/utils/useGetImmer';
 import { WidgetLibControllerFindAll } from '@/services/server/WidgetLibController';
 import { WidgetLibIncludeUserAndGroupIncludeWidgetsAndLicense } from '@/typings/includes';
 import { useRequestInternal } from '@/helpers/useRequestInternal';
 import { useState } from 'react';
+import { useMemoizedFn } from 'ahooks';
 
 export default () => {
   const [widgetLibs, setWidgetLibs] =
-    useState<WidgetLibIncludeUserAndGroupIncludeWidgetsAndLicense[]>();
+    useGetImmer<WidgetLibIncludeUserAndGroupIncludeWidgetsAndLicense[]>();
 
   const { run, loading } = useRequestInternal(
     async (name?: string) => {
       return WidgetLibControllerFindAll({
         includes: [
-          {
-            model: 'User',
-          },
+          { model: 'User' },
           {
             model: 'WidgetGroup',
             include: [
@@ -22,6 +22,7 @@ export default () => {
               },
             ],
           },
+          { model: 'License' },
         ],
       }) as Promise<WidgetLibIncludeUserAndGroupIncludeWidgetsAndLicense[]>;
     },
@@ -32,9 +33,19 @@ export default () => {
     },
   );
 
+  const addLicenseToItem = useMemoizedFn((id: string, license: API.License) => {
+    setWidgetLibs((draft) => {
+      const target = draft?.find((item) => item.id === id);
+      if (target) {
+        target.licenses.push(license);
+      }
+    });
+  });
+
   return {
     widgetLibs,
     requestDataSource: run,
+    addLicenseToItem,
     loading,
   };
 };
--- view/src/pages/WidgetsInstall/Markets/TableView/WidgetLibList.tsx
+++ view/src/pages/WidgetsInstall/Markets/TableView/WidgetLibList.tsx
@@ -1,4 +1,7 @@
+import { ProButton } from '@/components/ProButton';
 import useFetchIfUndefinedWhenMounted from '@/hooks/useFetchIfUndefinedWhenMounted';
+import useLoginUser from '@/hooks/useLoginUser';
+import { LicenseControllerCreate } from '@/services/server/LicenseController';
 import { WidgetLibIncludeUserAndGroupIncludeWidgetsAndLicense } from '@/typings/includes';
 import { ProList } from '@ant-design/pro-components';
 import { useModel } from '@umijs/max';
@@ -7,32 +10,19 @@ import { useMemo } from 'react';
 import Highlighter from 'react-highlight-words';
 import Filter from './_components/Filter';
 
-const renderBadge = (count?: number, active = false) => {
-  return (
-    <Badge
-      count={count}
-      style={{
-        marginTop: -3,
-        marginLeft: 4,
-        color: active ? '#1890FF' : '#999',
-        backgroundColor: active ? '#E6F7FF' : '#eee',
-      }}
-    />
-  );
-};
-
-
 export default () => {
   const { searchVal } = useModel('widgetsMarket.searchValue', (model) => ({
     searchVal: model.searchVal,
   }));
-  const { widgetLibs, requestDataSource } = useModel(
+  const { widgetLibs, requestDataSource, addLicenseToItem } = useModel(
     'widgetsMarket.tableList.widgetLibs',
     (model) => ({
       widgetLibs: model.widgetLibs,
       requestDataSource: model.requestDataSource,
+      addLicenseToItem: model.addLicenseToItem,
     }),
   );
+  const user = useLoginUser();
 
   const result = useMemo(() => {
     return searchVal
@@ -55,7 +45,9 @@ export default () => {
           title: '名称',
           dataIndex: 'name',
           render(dom, item) {
-            const total = (it: WidgetLibIncludeUserAndGroupIncludeWidgetsAndLicense) => {
+            const total = (
+              it: WidgetLibIncludeUserAndGroupIncludeWidgetsAndLicense,
+            ) => {
               let count = 0;
               for (let iit of it.widgetGroups) {
                 count += iit.widgets.length;
@@ -90,6 +82,36 @@ export default () => {
         subTitle: {
           title: '标签',
           dataIndex: 'labels',
+        },
+        actions: {
+          render(dom, entity, index, action, schema) {
+            const license = entity.licenses.find(
+              (lic) => lic.userId === user.id && lic.widgetLibId === entity.id,
+            );
+
+            return (
+              <ProButton
+                style={{
+                  width: 80,
+                }}
+                disabled={!!license}
+                size="small"
+                type="link"
+                key={'get'}
+                request={async () => {
+                  const license = await LicenseControllerCreate({
+                    widgetLibId: entity.id,
+                  });
+                  return license;
+                }}
+                onReqSuccess={(data) => {
+                  addLicenseToItem(entity.id, data);
+                }}
+              >
+                {!!license ? '已安装' : '免费安装'}
+              </ProButton>
+            );
+          },
         },
       }}
       toolBarRender={() => {
